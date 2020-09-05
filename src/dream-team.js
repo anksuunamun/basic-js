bcbb6b838d6808fb20cee3cb927f227b5c8fda97
anksuunamun <shishkina_veronika_10@mail.ru>
2020-09-05 17:49:17 +0300

    feat: add dream team decision

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let teamName = [];
+
+    for (let item of members) {
+      if (typeof (item) === "string") {
+        let newstr = item.trim();
+        if (newstr.indexOf(" ") !== -1) {
+          let whitespaceString = newstr.split(" ");
+          teamName.push((whitespaceString[0]).charAt(0));
+        } else if (newstr.indexOf("_") !== -1) {
+          let whitespaceString = newstr.split("_");
+          teamName.push((whitespaceString[0]).charAt(0));
+        } else {
+          teamName.push(item.charAt(0));
+        }
+      }
+    }
+    return teamName.join("").toUpperCase().split("").sort().join("");
+  }
+  return false;
 };
