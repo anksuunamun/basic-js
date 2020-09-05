5f9b5a39dd0d2ee710cc4e3eba6d705d6510242f
anksuunamun <shishkina_veronika_10@mail.ru>
2020-09-05 16:59:44 +0300

    feat: add carbon dating decision

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const K = 0.693 / HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if ((typeof (sampleActivity) === "string" && (typeof (+sampleActivity) === "number")) && +sampleActivity < 15 && +sampleActivity > 0) {
+    let t;
+    t = Math.log(MODERN_ACTIVITY / (+sampleActivity)) / K;
+    return Math.ceil(t);
+  }
+  return false;
+
 };
