4744a3b408014615d1fbcc791f953daaeb897494
anksuunamun <shishkina_veronika_10@mail.ru>
2020-09-06 11:20:56 +0300

    feat: add transform-array decision

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    let arrCopy = arr.slice();
+    let resultArray = [];
+    startNewSearch:
+      for (let i = 0; i < arrCopy.length; i++) {
+        if (arrCopy[i] !== '--discard-next' && arrCopy[i] !== '--double-prev' && arrCopy[i] !== '--discard-prev' && arrCopy[i] !== '--double-next') {
+          if ((arrCopy[i - 1] === '--discard-next' && arrCopy[i + 1] === '--double-prev') || (arrCopy[i - 1] === '--discard-next' && arrCopy[i + 1] === '--discard-prev')) {
+            continue startNewSearch;
+          } else if ((arrCopy[i - 1] === '--double-next' && arrCopy[i + 1] === '--discard-prev')) {
+            resultArray.push(arrCopy[i]);
+            continue startNewSearch;
+          } else if ((arrCopy[i - 1] === '--double-next' && arrCopy[i + 1] === '--double-prev')) {
+            resultArray.push(arrCopy[i], arrCopy[i], arrCopy[i]);
+            continue startNewSearch;
+          } else if (arrCopy[i - 1] === '--discard-next') {
+            continue startNewSearch;
+          } else if (arrCopy[i - 1] === '--double-next') {
+            resultArray.push(arrCopy[i], arrCopy[i])
+            continue startNewSearch;
+          } else if (arrCopy[i + 1] === '--discard-prev') {
+            continue startNewSearch;
+          } else if (arrCopy[i + 1] === '--double-prev') {
+            resultArray.push(arrCopy[i], arrCopy[i])
+            continue startNewSearch;
+          }
+          resultArray.push(arrCopy[i]);
+        } else if (arrCopy[i] === '--discard-next' || arrCopy[i] === '--double-prev' || arrCopy[i] === '--discard-prev' || arrCopy[i] === '--double-next') {
+          continue startNewSearch;
+        }
+      }
+
+
+    return resultArray;
+  } else {
+    throw new Error("Error");
+  }
+
 };
