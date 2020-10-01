744810267b6c64b4d9c2ef42cf87b0232e2ee4ac
anksuunamun <shishkina_veronika_10@mail.ru>
2020-10-01 19:08:18 +0300

    feat: add extended repeater decision

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+module.exports = function repeater(str, options) {
+  let resultArray = [];
+  let separator = options.separator || "+";
+  let additionSeparator = options.additionSeparator || "|";
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.additionRepeatTimes === undefined && options.addition) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.repeatTimes || 1) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      if (i < options.repeatTimes - 1) {
+        resultArray.push(String(str))
+        if (String(options.addition)) {
+          if (options.additionRepeatTimes || 1) {
+            for (let j = 0; j < options.additionRepeatTimes; j++) {
+              if (j < options.additionRepeatTimes - 1) {
+                resultArray.push(String(options.addition), additionSeparator);
+              } else {
+                resultArray.push(String(options.addition));
+              }
+            }
+          }
+        }
+        resultArray.push(separator)
+      }
+      else  {
+        resultArray.push(String(str))
+        if (String(options.addition)) {
+          if (options.additionRepeatTimes || 1) {
+            for (let j = 0; j < options.additionRepeatTimes; j++) {
+              if (j < options.additionRepeatTimes - 1) {
+                resultArray.push(String(options.addition), additionSeparator);
+              } else {
+                resultArray.push(String(options.addition));
+              }
+            }
+          }
+        }
+      } 
+    }
+  }
+  return resultArray.join("")
+
+
+
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
-  
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.calculateDepth = this.calculateDepth.bind(this);
+    this.depth = 1
   }
-};
+
+  calculateDepth(arr) {
+    Array.prototype.max = function() {
+      return Math.max.apply(null, this);
+    };
+    function findDepth(arr, depth = 0){
+      if (Array.isArray(arr)){
+        return arr.map((value) => {
+          return findDepth(value, depth+1)
+        } ).max()
+      }
+      
+      return depth;
+    }
+
+    return findDepth(arr)
+    
+  }
+}
